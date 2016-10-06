3b844b64a24318297b6ac5965049c2313ffab7ec
Tjitske Lohstroh <tjitske@lohstroh.nl>
2016-10-06 21:01:45 +0200

    Routes for individual actions replaced with a resources statement.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  Rails.application.routes.draw do
-    root "dinosaurs#index"
 
-    get "dinosaurs" => "dinosaurs#index"
-    get 'dinosaurs/new' => "dinosaurs#new"
-    get "dinosaurs/:id" => "dinosaurs#show", as: :dinosaur
+  # The many routes for individual actions can be replaced with a resources statement.
+  resources :dinosaurs
+  # get 'dinosaurs' => 'dinosaurs#index'
+  # get 'dinosaurs/new' => "dinosaurs#new", as: :new_dinosaur
+  # get 'dinosaurs/:id/edit' => 'dinosaurs#edit', as: :edit_dinosaur
+  # get 'dinosaurs/:id' => 'dinosaurs#show', as: :dinosaur
+  # post 'dinosaurs' => "dinosaurs#create"
+  # patch 'dinosaurs/:id' => "dinosaurs#update"
+  # delete 'dinosaurs/:id' => "dinosaurs#destroy"
 
-    resources :dinosaurs
-  end
+  root "dinosaurs#index"
+
 end
