3e04420446bdafbc12e830c9a1f8ab8ddf3cd8f1
Tjitske Lohstroh <tjitske@lohstroh.nl>
2016-10-06 20:55:56 +0200

    Refactor dinosaur params + Add Destroy action method to Dinosaurs controller.

--- app/controllers/dinosaurs_controller.rb
+++ app/controllers/dinosaurs_controller.rb
@@ -12,7 +12,6 @@ class DinosaursController < ApplicationController
   end
 
   def create
-      dinosaur_params = params.require( :dinosaur ).permit( :name, :age, :image_url )
 
       @dinosaur = Dinosaur.new( dinosaur_params )
 
@@ -29,17 +28,29 @@ class DinosaursController < ApplicationController
 
    # UPDATE *******************************
 
-   def update
-  @dinosaur = Dinosaur.find( params[:id] )
+  def update
+    @dinosaur = Dinosaur.find( params[:id] )
 
-  dinosaur_params = params.require( :dinosaur ).permit( :name, :age, :image_url )
-
-  if @dinosaur.update_attributes( dinosaur_params )
+    if @dinosaur.update_attributes( dinosaur_params )
      redirect_to @dinosaur
-  else
+    else
      render 'edit'
+    end
+  end #update
+
+  def destroy
+    @dinosaur = Dinosaur.find(params[:id])
+
+    @dinosaur.destroy
+
+    redirect_to dinosaurs_path
   end
-end
+
+ private
+
+  def dinosaur_params
+  params.require(:dinosaur).permit(:name, :age, :image_url)
+  end
 
 
 
