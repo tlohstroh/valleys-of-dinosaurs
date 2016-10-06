f7d141a6f12273c164513c672472455d6cef9dc1
Tjitske Lohstroh <tjitske@lohstroh.nl>
2016-10-06 16:12:55 +0200

    Add Update action method to Dinosaurs controller, to handle edit form submit.

--- app/controllers/dinosaurs_controller.rb
+++ app/controllers/dinosaurs_controller.rb
@@ -27,5 +27,20 @@ class DinosaursController < ApplicationController
       @dinosaur = Dinosaur.find(params[:id])
    end
 
+   # UPDATE *******************************
+
+   def update
+  @dinosaur = Dinosaur.find( params[:id] )
+
+  dinosaur_params = params.require( :dinosaur ).permit( :name, :age, :image_url )
+
+  if @dinosaur.update_attributes( dinosaur_params )
+     redirect_to @dinosaur
+  else
+     render 'edit'
+  end
+end
+
+
 
 end
